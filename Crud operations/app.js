e36c09170d662e86b92f601e653201fb5c34e787
TheTubbyRaider <grsraider66@gmail.com>
2024-02-20 22:25:43 -0700

    edits

--- Crud operations/app.js
+++ Crud operations/app.js
@@ -30,3 +30,101 @@ app.use('/delete', passport.authenticate('jwt', {session: false}), deleteRoutes)
 app.listen(3000);
 
 
+// Add OAuth authentication
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+// Implement CRUD operations for books, authors, genres
+const Book = require('./models/Book');
+const Author = require('./models/Author');
+const Genre = require('./models/Genre');
+
+// GET all books
+app.get('/books', (req, res) => {
+    Book.find()
+        .then(books => res.json(books))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// GET book by ID
+app.get('/books/:id', (req, res) => {
+    Book.findById(req.params.id)
+        .then(book => res.json(book))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// POST new book
+app.post('/books', (req, res) => {
+    const newBook = new Book(req.body);
+
+    newBook.save()
+        .then(() => res.json('Book added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Similar CRUD routes for authors and genres
+
+// Set up MongoDB collections
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://localhost:27017/bookdb';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+// Add controller logic
+const bookController = require('./controllers/bookController');
+app.use('/books', bookController);
+
+const authorController = require('./controllers/authorController');
+app.use('/authors', authorController);
+
+const genreController = require('./controllers/genreController');
+app.use('/genres', genreController);
+
+
+// Write tests
+const request = require('supertest');
+
+describe('Book API', () => {
+    it('gets all books', async () => {
+        const res = await request(app).get('/books');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toBeInstanceOf(Array);
+    });
+
+    it('gets a book by id', async () => {
+        const res = await request(app).get('/books/123');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toBeInstanceOf(Object);
+    });
+
+    // Add more tests for other routes
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+});
--- Final Project/config/db.js
+++ Final Project/config/db.js
@@ -15,3 +15,136 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+const mongoose = require('mongoose');
+
+const mongoUrl = process.env.MONGODB_URL;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
+
+const User = require('../models/User');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ message: 'User not found' });
+  }
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById
+}
+
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Book Library API',
+      version: '1.0.0'
+    }
+  },
+  apis: ['./routes/*.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// database.js
+
+const mongoose = require('mongoose');
+
+const mongoUrl = process.env.MONGODB_URL;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
+
+// users.js
+
+const User = require('../models/User');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ message: 'User not found' });
+  }
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById
+}
+
+
+// swagger.js
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Book Library API',
+      version: '1.0.0'
+    }
+  },
+  apis: ['./routes/*.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
